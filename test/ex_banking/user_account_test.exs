37e416ac91c857966f503776b16458f7585c5e2c
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-08-16 16:42:38 -0300

    user_account: fix some tests

--- test/ex_banking/user_account_test.exs
+++ test/ex_banking/user_account_test.exs
@@ -6,11 +6,12 @@ defmodule ExBanking.UserAccountTest do
   alias ExBanking.UserAccount
   doctest UserAccount
 
-  test "create user should return ok" do
-    assert UserAccount.create_user("vinicius") == :ok
+  test "create user that already existis should return an error" do
+    UserAccount.create_user("ronaldo")
+    assert UserAccount.create_user("ronaldo") == {:error, :user_already_exists}
   end
 
-  test "create user that already existis should return an error" do
-    assert UserAccount.create_user("vinicius") == {:error, :user_already_exists}
+  test "create user should return ok" do
+    assert UserAccount.create_user("vinicius") == :ok
   end
 end
