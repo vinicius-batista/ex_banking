af2d4da0b77b9c5de5a04b27e8bc69c96bd28aeb
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-08-17 10:14:07 -0300

    helpers: refactor in check_too_many_requests

--- lib/ex_banking/helpers.ex
+++ lib/ex_banking/helpers.ex
@@ -11,12 +11,13 @@ defmodule ExBanking.Helpers do
   def user_exists?(_), do: true
 
   def check_too_many_requests(pid) do
-    {_, len} = Process.info(pid, :message_queue_len)
-    has_more_than_10_requests(len)
+    pid
+    |> Process.info(:message_queue_len)
+    |> has_more_than_10_requests()
   end
 
-  defp has_more_than_10_requests(len) when len > 10, do: {:error, :too_many_requests_to_user}
-  defp has_more_than_10_requests(len), do: {:ok, len}
+  defp has_more_than_10_requests({_, len}) when len > 10, do: {:error, :too_many_requests_to_user}
+  defp has_more_than_10_requests({_, len}), do: {:ok, len}
 
   def format_money(money) do
     money
